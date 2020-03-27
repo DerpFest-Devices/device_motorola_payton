a04b37e595925a1bf0554d70ee1a48d36aec249d
trpsl <trpsl755@gmail.com>
2020-03-27 15:07:47 +0100

    payton: Track and include Moto Camera 2

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/motorola/payton/payton-vendor.mk)
 
+# MotoCamera2
+$(call inherit-product-if-exists, packages/apps/MotoCamera2/motocamera2.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
